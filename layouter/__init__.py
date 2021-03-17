88a1db4accd2853960cc1511ee8f74214c6cb714
KisaragiAyano <kisaragiayanoo@gmail.com>
2021-03-18 00:40:48 +0900

    Comment out icon file operation

--- layouter/__init__.py
+++ layouter/__init__.py
@@ -10,42 +10,47 @@ bl_info = {
     "category" : "Interface"
 }
 
+ICON_NAME = 'ops.interface.layouter'
 
 import bpy
 
 from .ops import register as ops_reg
 from .ops import unregister as ops_ureg
 
-import os
-from shutil import copyfile
 
-ICON_NAME = 'nikukikai.layouter'
+# import os
+# from shutil import copyfile
 
-def _copy_icon():
-    src = os.path.join(
-        os.path.abspath(os.path.dirname(__file__)), "icons", ICON_NAME+".dat")
-    blpath = os.path.split(bpy.app.binary_path)[0]
-    ver = f'{bpy.app.version[0]}.{bpy.app.version[1]}'
-    dst = os.path.join(blpath, ver, "datafiles", "icons", ICON_NAME+".dat")
+# def _copy_icon():
+#     src = os.path.join(
+#         os.path.abspath(os.path.dirname(__file__)), "icons", ICON_NAME+".dat")
+#     blpath = os.path.split(bpy.app.binary_path)[0]
+#     ver = f'{bpy.app.version[0]}.{bpy.app.version[1]}'
+#     dst = os.path.join(blpath, ver, "datafiles", "icons", ICON_NAME+".dat")
 
-    if os.path.isfile(src):
-        try:
-            copyfile(src, dst)
-        except:
-            print("Icon file not copied!")
+#     if os.path.isfile(src):
+#         try:
+#             copyfile(src, dst)
+#         except:
+#             pass
 
-def _remove_icon():
-    blpath = os.path.split(bpy.app.binary_path)[0]
-    ver = f'{bpy.app.version[0]}.{bpy.app.version[1]}'
-    path = os.path.join(blpath, ver, "datafiles", "icons", ICON_NAME+".dat")
+# def _remove_icon():
+#     blpath = os.path.split(bpy.app.binary_path)[0]
+#     ver = f'{bpy.app.version[0]}.{bpy.app.version[1]}'
+#     path = os.path.join(blpath, ver, "datafiles", "icons", ICON_NAME+".dat")
 
-    if os.path.isfile(path):
-        os.remove
+#     if os.path.isfile(path):
+#         try:
+#             os.remove(path)
+#         except:
+#             pass
 
 
 def register():
-    _copy_icon()
+    # _copy_icon()
     ops_reg()
 
 def unregister():
+    bpy.ops.wm.tool_set_by_id(name='builtin.move', space_type='VIEW_3D')
     ops_ureg()
+    # _remove_icon()
